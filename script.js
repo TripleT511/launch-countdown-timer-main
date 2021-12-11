202cd446ac3ec1f29740d3dfbe45fca21081eeca
TripleT511 <0306191074@caothang.edu.vn>
2021-12-11 10:36:27 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const dayId = document.querySelector("#day");
+const hourId = document.querySelector("#hour");
+const minuteId = document.querySelector("#minute");
+const secondId = document.querySelector("#second");
+
+
+const countDown = () => {
+    /* ============================ */
+    const newYear = new Date('February 1, 2022 23:59:59').getTime();
+    const toDay = new Date().getTime();
+    const gap = newYear - toDay;
+
+    /* ============================= */
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    /* ============================= */
+    const newDay = Math.floor(gap / day);
+    const newHour = Math.floor((gap % day) / hour);
+    const newMinute = Math.floor((gap % hour) / minute);
+    const newSecond = Math.floor((gap % minute) / second);
+
+    /* ============================= */
+    dayId.innerHTML = newDay;
+    hourId.innerHTML = newHour;
+    minuteId.innerHTML = newMinute;
+    secondId.innerHTML = newSecond;
+}
+
+/* ============= Render UI ============ */
+setInterval(() => {
+    countDown();
+}, 1000);
